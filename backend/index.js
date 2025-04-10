113dc1910ca852657e9d4ba1c1e1a97e849bb78f
Arish <mohdarish062@gmail.com>
2025-04-10 23:52:52 +0530

    Initial frontend commit

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,7 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: "Internal Server Error" });
-  });
+
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes); 
 
